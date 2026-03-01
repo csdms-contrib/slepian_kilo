a8660d4124cabe23874926d2f7df3805a1e4a6e6
Frederik J. Simons <fjsimons@gmail.com>
2026-03-01 13:25:09 -0500

    Calculates the coherence-square halfway point

--- transl.m
+++ transl.m
@@ -1,5 +1,5 @@
-function [k12,l12]=transl(f,Te,r,dr,E,v,g)
-% [k12,l12]=TRANSL(f,Te,r,dr,E,v,g)
+function varargout=transl(f,Te,r,dr,E,v,g)
+% [k12,l12,E,v]=TRANSL(f,Te,r,dr,E,v,g)
 %
 % Calculates the coherence-square halfway point in function 
 % of Forsyth's subsurface-to-surface loading ratio and the elastic
@@ -7,7 +7,7 @@ function [k12,l12]=transl(f,Te,r,dr,E,v,g)
 %
 % INPUT:
 %
-% f         Loading ratio [dimensionless]
+% f         Amplitude loading ratio [dimensionless, not squared]
 % Te        Elastic thickness [km], can be vector
 % r         Density [kg/m3]
 % dr        Density contrast [kg/m3]
@@ -19,17 +19,22 @@ function [k12,l12]=transl(f,Te,r,dr,E,v,g)
 %
 % k12       Wavenumber at which C2=0.5, in rad/km
 % l12       Wavelength at which C2=0.5, in km
+% E         Young's modulus
+% v         Poisson's ratio
 %
-% Last modified by fjsimons-at-alum.mit.edu, 04/06/2010
+% SEE ALSO: FORSYTH, COHERETE, COHEREF2
+% 
+% Last modified by fjsimons-at-alum.mit.edu, 03/05/2012
 
 defval('f',0.5)
 defval('Te',17)
 defval('E',1.4e11);
 defval('v',0.25);
 defval('r',2670);
-defval('dr',330);
+defval('dr',630);
 defval('g',fralmanac('GravAcc'));
 
+% Convert to standard units [m]
 Te=Te*1000;
 
 disp(sprintf('E= %5.3g; v= %5.3f',E,v))
@@ -56,10 +61,13 @@ k12=1./2.*2.^(3./4)./DD./FF.*...
 beta=(FF.^2.*dr.^2+4.*FF.^2.*dr.*r-2.*FF.^3.*dr.*r+...
        2.*FF.*dr.^2+FF.^2.*r.^2+2.*FF.^3.*r.^2-...
        2.*FF.*r.*dr+FF.^4.*r.^2+dr.^2);
+
 % Next line has the typo
 %k12=(2*g./DD./FF.*(-FF.*dr-FF.*r+FF.^2.*r+dr+beta.^(1/2))).^(1./4);
-% And note the typo, this should be corrected to
+
+% And noting the typo, this should be corrected to
 k=(1/2*g./DD./FF.*(-FF.*dr-FF.*r+FF.^2.*r+dr+beta.^(1/2))).^(1./4);
+
 % Check the results
 difer(k-k12,[],[],NaN)
 
@@ -72,3 +80,6 @@ difer(((1+DD.*k.^4/dr/g)*dr^2+FF.^2*r^2.*(1+DD.*k.^4/r/g)).^2./...
 k12=k12*1000;
 l12=2*pi./k12;
 
+% Produce output
+varns={k12,l12,E,v};
+varargout=varns(1:nargout);
