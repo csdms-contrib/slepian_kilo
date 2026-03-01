f2290e349cc5f0812f19cb54cd1bb6c9b9246ebe
Frederik J. Simons <fjsimons@gmail.com>
2026-03-01 13:23:31 -0500

    Calculates the coherence-square halfway point

--- transl.m
+++ transl.m
@@ -1,5 +1,5 @@
-function [k12,l12]=transl(f,Te,r,dr,E)
-% [k12,l12]=TRANSL(f,Te,r,dr,E)
+function [k12,l12]=transl(f,Te,r,dr,E,v,g)
+% [k12,l12]=TRANSL(f,Te,r,dr,E,v,g)
 %
 % Calculates the coherence-square halfway point in function 
 % of Forsyth's subsurface-to-surface loading ratio and the elastic
@@ -7,11 +7,13 @@ function [k12,l12]=transl(f,Te,r,dr,E)
 %
 % INPUT:
 %
-% f         Loading ratio
-% Te        Elastic thickness in km, can be vector
-% r         Density in kg/m3
-% dr        Density contrast in kg/m3
-% E         Young's modulus
+% f         Loading ratio [dimensionless]
+% Te        Elastic thickness [km], can be vector
+% r         Density [kg/m3]
+% dr        Density contrast [kg/m3]
+% E         Young's modulus [Pa]
+% v         Poisson's ratio [dimensionless]
+% g         Gravitational acceleration [m/s2]
 %
 % OUTPUT:
 %
@@ -26,7 +28,7 @@ defval('E',1.4e11);
 defval('v',0.25);
 defval('r',2670);
 defval('dr',330);
-g=fralmanac('GravAcc');
+defval('g',fralmanac('GravAcc'));
 
 Te=Te*1000;
 
@@ -50,5 +52,23 @@ k12=1./2.*2.^(3./4)./DD./FF.*...
        2.*FF.*r.*dr+FF.^4.*r.^2+dr.^2).^(1./2))...
      .*g.*DD.^3.*FF.^3).^(1./4);
 
+% Now do it exactly as it appears in the EPSL paper:
+beta=(FF.^2.*dr.^2+4.*FF.^2.*dr.*r-2.*FF.^3.*dr.*r+...
+       2.*FF.*dr.^2+FF.^2.*r.^2+2.*FF.^3.*r.^2-...
+       2.*FF.*r.*dr+FF.^4.*r.^2+dr.^2);
+% Next line has the typo
+%k12=(2*g./DD./FF.*(-FF.*dr-FF.*r+FF.^2.*r+dr+beta.^(1/2))).^(1./4);
+% And note the typo, this should be corrected to
+k=(1/2*g./DD./FF.*(-FF.*dr-FF.*r+FF.^2.*r+dr+beta.^(1/2))).^(1./4);
+% Check the results
+difer(k-k12)
+
+% Also check by plugging it in
+difer(((1+DD.*k.^4/dr/g)*dr^2+FF.^2*r^2.*(1+DD.*k.^4/r/g)).^2./...
+       ((1+DD.*k.^4/dr/g).^2*dr^2+FF.^2*r^2)./...
+        (dr^2+FF.^2*r^2.*(1+DD.*k.^4/r/g).^2)-1/2)
+
+% Back into the right units
 k12=k12*1000;
 l12=2*pi./k12;
+
