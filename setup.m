e4b9770e549ff622e504588e90b8e84c532d3a92
Erin ONeil <eo6288@princeton.edu>
2026-03-17 01:14:24 -0400

    Clean broken submodule state

--- setup.m
+++ setup.m
@@ -1,7 +1,13 @@
 function setup()
+    % Add main source code
     addpath(genpath('src'));
 
-    if isfolder('external')
-        addpath(genpath('external'));
+    % Add external dependencies (if present)
+    if isfolder('externals')
+        addpath(genpath('externals'));
     end
+
+    % Check for required toolbox
+    assert(license('test', 'Optimization_Toolbox') == 1, ...
+        'Optimization Toolbox is required.');
 end
