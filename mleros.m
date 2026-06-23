ce032444aff5bbb819970cbdc0f50ac7ce27a8b8
Frederik J. Simons <fjsimons@gmail.com>
2026-06-23 17:50:31 -0400

    The main Maximum-Likelihood Estimation routine

--- mleros.m
+++ mleros.m
@@ -307,7 +307,7 @@ elseif strcmp(Hx,'demo1')
 
     % Initialize the THZRO file... note that the bounds may change
     % between simulations, and only one gets recorded here
-    if ~any(isnan(thhat)) && index==1 && labindex==1
+    if ~any(isnan(thhat)) && index==1 % && labindex==1
       oswzerob(fids(1),th0,p,lpars,fmts)
     end
 
